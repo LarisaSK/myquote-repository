56384a3142ec8c0f3451c30c39269a816e1e031c
Larisa Saitova <sklarisa@hotmail.com>
2022-02-07 13:14:15 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/LarisaSK/myquote-repo/myquote"
+)
+
+//henter funsjonene fra rsc.io/quote
+
+func main() {
+	myquote.Glass()
+
+	myquote.Go()
+
+	myquote.Hello()
+
+	myquote.Opt()
+}
